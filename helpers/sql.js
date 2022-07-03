e33ca1d16fa9c4f0f7094d2e195793b3beca4303
Tony Alvarez <tonyalva87@gmail.com>
2022-07-02 17:48:29 -0700

    add jobs model and tests

--- helpers/sql.js
+++ helpers/sql.js
@@ -1,38 +1,38 @@
 const { BadRequestError } = require("../expressError");
 
 /** Convert partial JSON data into SQL query
- * 
+ *
  * This converts JSON data passed from the req.body into a SQL-friendly
  * formatted return.
- * 
+ *
  * First, it converts any non-matching keys into its corresponding SQL
  * column key.
- * 
+ *
  * Then it assigns a "$(int)" variable substitution for each key, to be
  * used as a variable substitution in the router.
- * 
+ *
  * Lastly, it returns an object that contains:
  * - setCols: the columns to be queried
  * - values: the values to update for each column
- * 
+ *
  * Arguments:
  * 1. dataToUpdate: JSON object of data to be queried
  * 2. jsToSql: JSON object that updates the inbound key with the matching
  * SQL column / key
- * 
+ *
  * Example: inbound data can be:
  * dataToUpdate = {"firstName": "testFName", "lastName": "testLName" }
- * 
+ *
  * Throws BadRequestError if no data is passed.
  */
 
-function sqlForPartialUpdate(dataToUpdate, jsToSql) {
+function sqlForPartialUpdate(dataToUpdate, jsToSql = {}) {
   const keys = Object.keys(dataToUpdate);
   if (keys.length === 0) throw new BadRequestError("No data");
 
   // {firstName: 'Aliya', age: 32} => ['"first_name"=$1', '"age"=$2']
-  const cols = keys.map((colName, idx) =>
-      `"${jsToSql[colName] || colName}"=$${idx + 1}`,
+  const cols = keys.map(
+    (colName, idx) => `"${jsToSql[colName] || colName}"=$${idx + 1}`
   );
 
   return {
--- helpers/sql.test.js
+++ helpers/sql.test.js
@@ -1,29 +1,48 @@
 const { sqlForPartialUpdate } = require("./sql");
 
 let dataToUpdate = {
-    firstName: "NewF",
-    lastName: "NewF",
-    email: "new@email.com",
-    isAdmin: true,
+  firstName: "NewF",
+  lastName: "NewF",
+  email: "new@email.com",
+  isAdmin: true,
+};
+
+let jobDataToUpdate = {
+  title: "new_j1",
+  salary: 175000,
+  equity: 0.011,
+  company_handle: "c1",
 };
 
 let jsToSql = {
-    firstName: "first_name",
-    lastName: "last_name",
-    isAdmin: "is_admin"
-}
+  firstName: "first_name",
+  lastName: "last_name",
+  isAdmin: "is_admin",
+};
 
 describe("sqlForPartialUpdate", function () {
-    test("works: updates data", function () {
-        let result = sqlForPartialUpdate(dataToUpdate, jsToSql)
-        let expectedResult = {
-            setCols: '"first_name"=$1, "last_name"=$2, "email"=$3, "is_admin"=$4',
-            values: ["NewF", "NewF", "new@email.com", true]
-        }
-        expect(result).toEqual(expectedResult)
-    });
-    test("works: throws error if no data", function () {
-        dataToUpdate = {};
-        expect(() => sqlForPartialUpdate(dataToUpdate, jsToSql)).toThrowError
-    });
-})
+  test("works: updates data", function () {
+    let result = sqlForPartialUpdate(dataToUpdate, jsToSql);
+    let expectedResult = {
+      setCols: '"first_name"=$1, "last_name"=$2, "email"=$3, "is_admin"=$4',
+      values: ["NewF", "NewF", "new@email.com", true],
+    };
+    expect(result).toEqual(expectedResult);
+  });
+  test("works: updates when no jsToSql object is empty", function () {
+    let result = sqlForPartialUpdate(jobDataToUpdate);
+    let expectedResult = {
+      setCols: '"title"=$1, "salary"=$2, "equity"=$3, "company_handle"=$4',
+      values: ["new_j1", 175000, 0.011, "c1"],
+    };
+    expect(result).toEqual(expectedResult);
+  });
+  test("works: throws error if no data", function () {
+    dataToUpdate = {};
+    expect(() => sqlForPartialUpdate(dataToUpdate, jsToSql)).toThrowError;
+  });
+  test("works: throws error if no data", function () {
+    dataToUpdate = {};
+    expect(() => sqlForPartialUpdate(dataToUpdate, jsToSql)).toThrowError;
+  });
+});
